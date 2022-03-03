376106dbae81aa25d368930e845e60608787148b
Christian Sailor <christian.sailor@gmail.coml>
2022-03-03 03:37:50 +0100

    final commit, I will become a React pro

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './Marcos.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+		My name is Marcos G. and I'm going to master React!
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
